9d0314d68e0c6bdd1f3bc7f75912293e6b5702ae
Shinnosuke Sawada <6warashi9@gmail.com>
2019-02-17 13:48:17 +0900

    add mas script

---
+++ scripts/mas.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# from: https://qiita.com/yumiduka/items/9c095b9f98be96b8763c
+
+APPID=$1
+
+## Argument check
+test $# -eq 1 || exit 2
+
+## App check
+mas list 2> /dev/null | grep -q "^${APPID} " && exit 0
+
+## App install
+mas install ${APPID} > /dev/null 2>&1 || exit 3
+
+## Normal termination
+exit 1
